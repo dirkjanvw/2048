5ba0292356c8b52511e80414c3ad5e24a1d7f0ee
dirkjanvw <dirk-janvanworkum@hotmail.nl>
2023-02-16 23:15:19 +0100

    got up and down movement going

--- 2048.py
+++ 2048.py
@@ -62,17 +62,85 @@ class Game:
         for tile in to_generate:
             self.board[tile[0]][tile[1]] = 2
 
-    def play(self) -> None:
+    def play(self, move: str) -> None:
+        """
+        Plays a move in the 2048 game.
+
+        Arguments:
+            move (str): h, j, k, or l meaning: left, down, up c.q. right
+        """
+
+        match move:
+            case 'h':
+                self.move_left()
+            case 'j':
+                self.move_down()
+            case 'k':
+                self.move_up()
+            case 'l':
+                self.move_right()
+
+    def move_left(self) -> None:
         """
         """
+
+    def move_right(self) -> None:
+        """
+        """
+
+        #for i in range(self.size):
+        #    new = [0 for k in range(self.size)]
+        #    p = self.size
+        #    for j in range(self.size):
+
+    def move_down(self) -> None:
+        """
+        """
+        for i in range(self.size):
+            new = [0 for k in range(self.size)]
+            p = self.size-1
+            for j in range(self.size):
+                if self.board[i][j] != 0:
+                    new[p] = self.board[i][j]
+                    p -= 1
+            self.board[i] = new
+
+    def move_up(self) -> None:
+        """
+        """
+        for i in range(self.size):
+            new = [0 for k in range(self.size)]
+            p = 0
+            for j in range(self.size):
+                if self.board[i][j] != 0:
+                    new[p] = self.board[i][j]
+                    p += 1
+            self.board[i] = new
+
+def get_input() -> str:
+    """
+    Gets user input for movement.
+
+    Returns:
+        str: h, j, k, or l meaning: left, down, up c.q. right
+    """
+
+    move = ''
+    while move not in ['h','j','k','l']:
+        move = input("next move (h/j/k/l): ")
+
+    return move
 
 def main():
     # Initialise game
     game = Game(4)
 
     # Play game
-    print(game)
-
+    while True:
+        print(game)
+        game.play(get_input())
+        #print(game)
+        game.generate()
 
 if __name__ == "__main__":
     main()
